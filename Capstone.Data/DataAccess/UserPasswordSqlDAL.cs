294acfd14c6905fcdef0f128b438b9232def913b
Kyle Pierson <piersonkyle90@yahoo.com>
2016-08-08 14:32:26 -0400

    yay

---
+++ Capstone.Data/DataAccess/UserPasswordSqlDAL.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Data.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Capstone.Data.DataAccess
+{
+    public class UserPasswordSqlDAL : IUserPasswordDAL
+    {
+        private string connectionString;
+        private const string SQL_GetUser = "SELECT * FROM user_password WHERE @username=username AND @password=password;";
+
+        public UserPasswordSqlDAL()
+            : this(ConfigurationManager.ConnectionStrings["CapstoneDatabaseConnection"].ConnectionString)
+        {
+        }
+
+        //Constructor
+        public UserPasswordSqlDAL(string databaseconnectionString)
+        {
+            connectionString = databaseconnectionString;
+        }
+
+        public UserPassword GetUser(string username, string password)
+        {
+            UserPassword output = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(SQL_GetUser, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        output = new UserPassword();
+                        output.CityId = Convert.ToInt32(reader["id"]);
+                        output.Name = Convert.ToString(reader["name"]);
+                        output.CountryCode = Convert.ToString(reader["countrycode"]);
+                        output.District = Convert.ToString(reader["district"]);
+                        output.Population = Convert.ToInt32(reader["population"]);
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Capstone.Web/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+
+        public ActionResult LogIn()
+        {
+            //username and password view model to send to view
+            LoginViewModel model = new LoginViewModel();
+            return View("Login", model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(string username, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Login");
+            }
+            //verify credentials, get role title and redirect to proper controller
+            return RedirectToAction("Index", model.title, model);
+        }
+    }
+}
