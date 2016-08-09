8245cb8410bb6dc25df8087505dafbad00a00712
Simin Nickelsen <simin_st7@yahoo.com>
2016-08-09 09:45:39 -0400

    student model and Istudent are done

---
+++ Capstone.Data/DataAccess/IStudentDAL.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Data.Models;
+
+namespace Capstone.Data.DataAccess
+{
+    public interface IStudentDAL
+    {
+        List<Student> GetAllStudents();
+    }
+}
