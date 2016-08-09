7c54b9b76388a9b0dce13bd0f852cc703906c81f
Simin Nickelsen <simin_st7@yahoo.com>
2016-08-08 21:36:50 -0400

    student view ....not finished

---
+++ Capstone.Web/Models/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class StudentModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Capstone.Data.Models.Students> AllStudents { get; set; }
+
+
+
+    }
+}
