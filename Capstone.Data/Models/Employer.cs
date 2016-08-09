a17292e0f7c8b7bcd38973e86f500593688fb083
Simin Nickelsen <simin_st7@yahoo.com>
2016-08-09 14:49:23 -0400

    Emlpoyer model is added

---
+++ Capstone.Data/Models/Employer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Data.Models
+{
+    public class Employer
+    {
+        public int EmployerId { get; set; }
+        public string Username { get; set; }
+        public string CompanyName { get; set; }
+        public string ContactFirstName { get; set; }
+        public string ContactLastName { get; set; }
+        public string ContactInfo { get; set; }
+        public string Address { get; set; }
+    }
+}
