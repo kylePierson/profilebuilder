fe7b2b644cbb7d38fced3db7cde81537b9bb3147
Simin Nickelsen <simin_st7@yahoo.com>
2016-08-11 12:16:00 -0400

    Employer data access layer

--- Capstone.Data/DataAccess/IEmployerDAL.cs
+++ Capstone.Data/DataAccess/IEmployerDAL.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Capstone.Data.Models;
 
 namespace Capstone.Data.DataAccess
 {
     public interface IEmployerDAL
     {
+        Employer GetEmployer(string username);
         bool AddEmployerUser(string username, string firstName, string lastName, string company, string programingLanguage);
+        //bool UpdateEmployer(string username);
+        bool AddInterest(string username, string interest);
+        bool DeleteInterest(string username, string interest);
     }
 }
--- Joins2.sql
+++ Joins2.sql
@@ -6,7 +6,7 @@ INNER JOIN student_softskills ON student.student_id = student_softskills.student
 INNER JOIN softskills ON student_softskills.softskill_id = softskills.softskill_id
 INNER JOIN student_interests ON student.student_id = student_interests.student_id
 INNER JOIN interests ON student_interests.interest_id = interests.interest_id
-
+Where  student.student_id =1;
 
 select * from student_softskills;
 select * from student;
@@ -15,5 +15,30 @@ select * from student_interests;
 select * from softskills;
 select * from academic;
 
+INSERT INTO interests (interest)
+VALUES ('Front end Develpoer');
 
+INSERT INTO interests (interest)
+VALUES ('Software Tester');
 
+INSERT INTO student_interests (student_id, interest_id)
+VALUES (1,2);
+
+INSERT INTO student_interests (student_id, interest_id)
+VALUES (2,1);
+
+SELECT student.firstname,student.lastname, student.summary, student.perviousexperience, student.class, student.contactinfo,
+academic.degree,academic.school, academic.major, softskills.skill, interests.interest
+FROM student
+INNER JOIN academic on student.student_id = academic.student_id
+INNER JOIN student_softskills ON student.student_id = student_softskills.student_id
+INNER JOIN softskills ON student_softskills.softskill_id = softskills.softskill_id
+INNER JOIN student_interests ON student.student_id = student_interests.student_id
+INNER JOIN interests ON student_interests.interest_id = interests.interest_id
+
+SELECT *
+FROM softskills
+
+select *
+from student_softskills
+INNER JOIN student_softskills ON softskills.softskill_id = student_softskills.softskill_id
