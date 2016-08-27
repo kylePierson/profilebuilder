e24975540a590e8cc463712d9a8d4e44fb49ee5c
Kyle W. Pierson <piersonkyle90@yahoo.com>
2016-08-27 14:31:29 -0400

    added caret swivels for drop down

---
+++ Capstone.Web/Content/Caret.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function () {
+
+    $("#skillsDropdown").on("click", function () {
+        var current = $("#skillsCaret");
+        if (current.hasClass("open")) {
+            current.removeClass("open");
+            console.log(current.hasClass("open"));
+        }
+        else {
+            current.addClass("open");
+            console.log(current.hasClass("open"));
+        }
+    });
+
+    $("#interestsDropdown").on("click", function () {
+        var current = $("#interestsCaret");
+        if (current.hasClass("open")) {
+            current.removeClass("open");
+            console.log(current.hasClass("open"));
+        }
+        else {
+            current.addClass("open");
+            console.log(current.hasClass("open"));
+        }
+    });
+
+    $("#summaryDropdown").on("click", function () {
+        var current = $("#summaryCaret");
+        if (current.hasClass("open")) {
+            current.removeClass("open");
+            console.log(current.hasClass("open"));
+        }
+        else {
+            current.addClass("open");
+            console.log(current.hasClass("open"));
+        }
+    });
+
+    $("#projectsDropdown").on("click", function () {
+        var current = $("#projectsCaret");
+        if (current.hasClass("open")) {
+            current.removeClass("open");
+            console.log(current.hasClass("open"));
+        }
+        else {
+            current.addClass("open");
+            console.log(current.hasClass("open"));
+        }
+    });
+
+    //document.getElementById('skillsDropdown').addEventListener('click', function () {
+    //    [].slice.call(document.getElementsById('skillsCaret')).forEach(function (arrow) {
+    //        arrow.classList.toggle('open');
+    //    });
+    //}, false);
+
+    //document.getElementById('dropdown').addEventListener('click', function () {
+    //    [].slice.call(document.getElementsById('skillsCaret')).forEach(function (arrow) {
+    //        arrow.classList.toggle('open');
+    //    });
+    //}, false);
+
+    //document.getElementById('dropdown').addEventListener('click', function () {
+    //    [].slice.call(document.getElementsById('skillsCaret')).forEach(function (arrow) {
+    //        arrow.classList.toggle('open');
+    //    });
+    //}, false);
+
+    //document.getElementById('dropdown').addEventListener('click', function () {
+    //    [].slice.call(document.getElementsById('skillsCaret')).forEach(function (arrow) {
+    //        arrow.classList.toggle('open');
+    //    });
+    //}, false);
+
+});
