411f9307250b12ee95171d1c46e32aa1cf10b510
Kyle W. Pierson <piersonkyle90@yahoo.com>
2016-09-04 21:07:50 -0400

    Modified javascript for update buttons

--- Capstone.Web/Content/AddNewInterest.js
+++ Capstone.Web/Content/AddNewInterest.js
@@ -3,9 +3,9 @@
         var newInterest = $("#newInterestTextbox").val();
         if (newInterest) {
             var username = $("#hiddenusername1").val();
-            var href = "/Student/DeleteInterest?skill=" + newInterest + "&username=" + username;
+            var href = "/Student/DeleteStudentInterest?interest=" + newInterest + "&username=" + username;
             var linkHtml = '<a href=' + href + '>Delete</a>';
-            $("#interestList").append("<li>" + newInterest + "&nbsp;&nbsp;&nbsp;" + linkHtml + "</li>")
+            $("#interestList").append("<li>" + newInterest + "&nbsp;&nbsp;&nbsp;" + '<span style="position:absolute; right:50px">' + linkHtml + "</span></li>")
             $.post("/Student/AddNewInterest", { username: username, addInterest: newInterest })
         }
 
--- Capstone.Web/Content/AddNewSkill.js
+++ Capstone.Web/Content/AddNewSkill.js
@@ -1,7 +1,13 @@
 ﻿$(document).ready(function () {
-    $("#addStaffButton").click(function () {
-            $.post("/Student/CreateStaffUser")           
-
+    $("#addNewSkill").click(function () {
+        var newSkill = $("#newSkillTextbox").val();
+        if (newSkill) {
+            var username = $("#hiddenusername").val();
+            var href = "/Student/DeleteStudentSkill?skill=" + newSkill + "&username=" + username;
+            var linkHtml = '<a href=' + href + '>Delete</a>';
+            $("#skillList").append("<li>" + newSkill + "&nbsp;&nbsp;&nbsp;" + '<span style="position:absolute; right:50px">' + linkHtml + "</span></li>")
+            $.post("/Student/AddNewSkill", { username: username, addSkill: newSkill })
+        }
     });
 
 });
